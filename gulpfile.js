d9d9d3f91f9cfdacfa6062076d6be15a9ebab782
Maksymilian Sleziak <maksymilian.sleziak@gmail.com>
2016-10-21 19:27:16 +0200

    gulp init

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
